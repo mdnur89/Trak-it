45f092e8fb9356b3366dd708dd405f3939888256
@mdnur89 <tatendabandawa01@gmail.com>
2024-08-24 06:27:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+Trak issues, track-it
+
+## ✨ Features
+
+- Team management
+- Dept management
+- Ticket management
+- User assignment
+- Dept statistics
+- Advanced searching
+- Ticket Comments
+- Role based organization
+- Custom field creation
+- Attachments (Not done)
+
+## 🚀 Quick start
+
+### Step 1: Install Dependencies
+
+cd into the client and server directory, and install the dependencies
+
+```sh
+cd client & npm install
+```
+
+```sh
+cd server & npm install
+```
+
+### Step 2: Setup .env
+
+To run the server, you will need the `.env` variables
+
+Rename [.env.EXAMPLE](./server/.env.EXAMPLE) file to **.env**
+
+### Step 3: Seed database
+
+Execute the following command to seed the database
+
+```sh
+npm run seed
+```
+
+#### You are done!
+
+Run client application
+
+```sh
+npm run dev
+```
+
+Run server application
+
+```sh
+npm run start
+```
---
+++ client/src/pages/auth.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import {
+  Button,
+  Heading,
+  Icon,
+  Link,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { AiFillGithub } from "react-icons/ai";
+import { Login } from "@/components/authentication/Login";
+import DemoLoginInfoModal from "@/components/others/DemoLoginInfoModal";
+import logo from "@/assets/Trackit_Plain.png";
+
+export const Auth = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <VStack p={5}>
+        <Link
+          href="https://github.com/mdnur89"
+          isExternal
+          alignSelf="flex-end"
+        >
+          <Icon as={AiFillGithub} w={8} h={8} />
+        </Link>
+        <Image width={300} src={logo} alt="logo" />
+        <Heading as="h3" size="md" pb="5" fontWeight="semibold">
+          Sign in to your account
+        </Heading>
+        <Login />
+        <br />
+        <Button onClick={onOpen}>Demo Login Info</Button>
+        <DemoLoginInfoModal isOpen={isOpen} onClose={onClose} />
+      </VStack>
+    </>
+  );
+};
+
+export default Auth;
